b0f221ac938bf81c1a9a8fd1291337980a436628
SutharsanL <sutharsanbe94@gmail.com>
2022-07-11 09:58:02 +0530

    first commit

---
+++ src/test/java/com/vcentry/lab/runner/TestRunner.java
@@ -0,0 +1,21 @@
+package com.vcentry.lab.runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+@RunWith(Cucumber.class) //junit main method
+@CucumberOptions(
+features="src/test/resources/features",
+glue="com.vcentry.lab.stepDefinition", //stepdefn pkg name
+plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+monochrome = true, // readable console
+dryRun = false //dry run used to check feature file steps 
+                  //were implemented in TestSteps
+//tags = "@validlogin or @sanity"
+//tags="@login"
+		)
+
+public class TestRunner {
+
+}
